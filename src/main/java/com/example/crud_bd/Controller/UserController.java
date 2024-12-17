2409969a7ff5cfb62e27710aa80d260a82918185
iam <aditamiloss@gmail.com>
2024-12-17 21:05:44 +0300

    create delete??? and create

--- src/main/java/com/example/crud_bd/Controller/UserController.java
+++ src/main/java/com/example/crud_bd/Controller/UserController.java
@@ -8,8 +8,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,4 +44,30 @@ public class UserController {
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
+
+    @PostMapping("/new")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        if(user == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        if(id == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        userService.deleteUserById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/")
+    public ResponseEntity<Void> deleteUserByPassport(@RequestBody String passport) {
+        if(passport == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        userService.deleteUserByPassport(passport);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/com/example/crud_bd/Repository/UserRepository.java
+++ src/main/java/com/example/crud_bd/Repository/UserRepository.java
@@ -2,18 +2,39 @@ package com.example.crud_bd.Repository;
 
 import com.example.crud_bd.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-@Query("SELECT u FROM User u WHERE u.id = ?1")
-User getUserById(int id);
+    User getUserByPassport(String passport);
 
-List<User> findAll();
-@Query("select u from User u where u.age > 18")
-List<User> findAllAdultUsers();
+    @Query("SELECT u FROM User u WHERE u.id = ?1")
+    User getUserById(int id);
+
+    List<User> findAll();
+
+    @Query("select u from User u where u.age > 18")
+    List<User> findAllAdultUsers();
+
+    @Query("select count(*) from User u where u.passport = ?1")
+    Integer contains(String passport);
+
+    User save(User user);
+
+    @Modifying
+    @Transactional
+    @Query("delete from User u where u.id=?1")
+    void deleteUserById(Long id);
+
+    @Modifying
+    @Transactional
+    @Query("delete from User u where u.passport=?1")
+    int deleteUserByPassport(String passport);
 }
