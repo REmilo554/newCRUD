9ce9bd2e5b3abe8760534cc7e789cc938207d366
iam <aditamiloss@gmail.com>
2024-12-17 21:05:44 +0300

    create delete??? and create

--- src/main/java/com/example/crud_bd/Entity/User.java
+++ src/main/java/com/example/crud_bd/Entity/User.java
@@ -4,11 +4,13 @@ package com.example.crud_bd.Entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @Entity
 @Table(name = "users")
@@ -17,13 +19,15 @@ import lombok.NoArgsConstructor;
 @Data
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private long id;
     @Column(name = "first_name", nullable = false)
-    private String first_name;
+    private String firstName;
     @Column(name = "second_name", nullable = false)
-    private String second_name;
+    private String secondName;
     @Column(name = "age", nullable = false)
     private int age;
+    @Column(name = "passport" , nullable = false)
+    private String passport;
 }
--- src/main/java/com/example/crud_bd/Service/UserService.java
+++ src/main/java/com/example/crud_bd/Service/UserService.java
@@ -3,13 +3,15 @@ package com.example.crud_bd.Service;
 import com.example.crud_bd.Entity.User;
 import com.example.crud_bd.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Slf4j
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -33,4 +35,31 @@ public class UserService {
     public List<User> getAllAdultUser() {
         return Optional.ofNullable(userRepository.findAllAdultUsers()).orElseThrow(RuntimeException::new);
     }
+
+
+    public User createUser(User user) {
+        if(user == null){
+            throw new RuntimeException("User not found");
+        }
+        return Optional.of(userRepository.save(user)).orElseThrow(RuntimeException::new);
+    }
+
+    public void deleteUserById(Long id) {
+        if(id == null)
+            throw new RuntimeException("User not found");
+        userRepository.deleteUserById(id);
+    }
+
+    //FIXME НЕ НАХОДИТ ПО ПАСПОРТУ
+    public void deleteUserByPassport(String passport) {
+        if(passport == null) {
+            throw new RuntimeException("Passport not found");
+        }
+        String d = passport.trim();
+        int i = userRepository.deleteUserByPassport(d);
+        log.atInfo().log("Deleting user by {} and {}", d,i);
+        User user = userRepository.getUserByPassport(d);
+        log.atInfo().log("Find user {}",user.toString());
+    }
+
 }
